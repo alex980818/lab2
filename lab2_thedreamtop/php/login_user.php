1e9eb8dc1bd2eeb198d686a3489f413765a12d43
Liang Hong <wonglianghong@hotmail.com>
2020-03-15 23:19:53 +0800

    php

---
+++ lab2_thedreamtop/php/login_user.php
@@ -0,0 +1,19 @@
+<?php
+error_reporting(0);
+include_once("dbconnect.php");
+$email = $_POST['email'];
+$password = sha1($_POST['password']);
+
+
+$sql = "SELECT * FROM USER WHERE EMAIL = '$email' AND PASSWORD = '$password' AND VERIFY = '1'";
+
+$result = $conn->query($sql);
+if ($result->num_rows > 0) {
+    while ($row = $result ->fetch_assoc()){
+        echo "success";
+    }
+}else{
+    echo "$sql";
+    echo "failed";
+}
+?>
